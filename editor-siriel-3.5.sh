84cab2650d0355d686624cc396a3e63e97cbb9cb
Juraj Michálek <juraj.michalek@gmail.com>
2022-05-14 10:15:12 +0200

    add source code of levels for Golden Ball

--- editor-siriel-3.5.sh
+++ editor-siriel-3.5.sh
@@ -8,7 +8,8 @@ dosbox \
     -c 'mount d siriel-3.5-dos' \
     -c 'd:' \
     -c 'cd d:\BIN' \
-    -c 'EDITOR.EXE 1.MIE' \
-    -c 'SI35.EXE /t 1.MIE' \
+    -c 'EDITOR.EXE' \
+    -c 'SI35.EXE /t' \
+    -c 'echo "type SI35.EXE /t to launch testing mode"' \
     -c 'echo "Type EXIT to close DosBox'
 
